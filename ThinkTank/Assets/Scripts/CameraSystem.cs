798f5ae876dce1d36afbe91c1fe2ee829beb0214
Morgan Lowe <Morgan.Lowe@mail.bcu.ac.uk>
2025-02-26 14:09:02 +0000

    sky sphere, and mouse controller camera
    still using old input system

---
+++ ThinkTank/Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    public Vector2 mouse_rotation;
+    public float sens;
+    void Start()
+    {
+        sens = 5.0f;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            mouse_rotation.x = (mouse_rotation.x + Input.GetAxis("Mouse X") * sens) % 360;
+            mouse_rotation.y = Mathf.Clamp((mouse_rotation.y + Input.GetAxis("Mouse Y") * sens), -90.0f, 90.0f); ;
+            transform.localRotation = Quaternion.Euler(-mouse_rotation.y, mouse_rotation.x, 0);
+        } 
+    }
+
+}
