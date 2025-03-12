75aec3dcf24ebb7752a80d4eacb9ad5ed2a3ebe0
Morgan Lowe <Morgan.Lowe@mail.bcu.ac.uk>
2025-03-12 12:56:22 +0000

    new input system + scrolling

--- ThinkTank/Assets/Scripts/CameraSystem.cs
+++ ThinkTank/Assets/Scripts/CameraSystem.cs
@@ -1,23 +1,49 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraSystem : MonoBehaviour
 {
+    public NewControls playerControls;
     public Vector2 mouse_rotation;
     public float sens;
-    void Start()
+
+    private Vector2 move_direction;
+    private float mouse_click;
+
+    private void Awake()
     {
-        sens = 5.0f;
+        sens = 1.0f;
+        playerControls = new NewControls();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
     }
 
     void Update()
     {
-        if (Input.GetMouseButton(0)) {
-            mouse_rotation.x = (mouse_rotation.x + Input.GetAxis("Mouse X") * sens) % 360;
-            mouse_rotation.y = Mathf.Clamp((mouse_rotation.y + Input.GetAxis("Mouse Y") * sens), -90.0f, 90.0f); ;
+        // get input values.
+        move_direction = playerControls.ActionMap1.Mouse.ReadValue<Vector2>();
+        mouse_click = playerControls.ActionMap1.MouseClick.ReadValue<float>();
+
+        // if clicking... (as this checks every frame this also is effectively checking for a hold click).
+        if (mouse_click == 1.0f) {
+            // get mouse position deltas to rotate around. Clamp the Y direction to not go upside down.
+            mouse_rotation.x = mouse_rotation.x + move_direction.x * sens % 360;
+            mouse_rotation.y = Mathf.Clamp((mouse_rotation.y + move_direction.y * sens), -90.0f, 90.0f);
+
+            // update the rotation.
             transform.rotation = Quaternion.Euler(-mouse_rotation.y, mouse_rotation.x, 0);
         }
+        // scale moves the camera closer to the pivot point based on mouse scroll.
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, 
+            Mathf.Clamp(transform.localScale.z - playerControls.ActionMap1.MouseScroll.ReadValue<float>() / 1200.0f, 0.2f, 2.0f)); // subtract the scroll distance to the scale and clamp.
     }
 }
--- ThinkTank/Assets/Scripts/ObjectClickEvents.cs
+++ ThinkTank/Assets/Scripts/ObjectClickEvents.cs
@@ -1,28 +1,48 @@
-using System;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
 
 public class ObjectClickEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    bool is_mouse = false;
+    float mouse_in = 0.0f;
     // Update is called once per frame
     void Update()
     {
-        
+        // Create a ray from the camera to the object under the mouse cursor
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Perform the raycast and check if it hits the object and it's not blocked
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Check if the ray hit the object this script is attached to
+            if (hit.collider.gameObject == this.gameObject)
+            {
+                // You are over the object, and it isn't blocked by another object
+                //MouseOver();
+            }
+        }
     }
 
     private void OnMouseEnter()
     {
-        
+        is_mouse = true;
+        mouse_in = Time.time;
     }
 
     private void OnMouseExit()
     {
-        
+        is_mouse = false;
+        mouse_in = Time.time;
+    }
+
+    private void OnMouseOver()
+    {
+        if (Time.time - mouse_in > 1.0f && is_mouse) { 
+            print("RAAH");
+            is_mouse = false;
+        }
     }
 }
