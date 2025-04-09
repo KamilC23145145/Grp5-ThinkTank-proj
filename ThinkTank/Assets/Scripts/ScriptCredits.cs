4253b9b59e932bb3554dd1d9e50e7df66387be92
KamilC23145145 <Kamil.Cychowski@mail.bcu.ac.uk>
2025-04-09 11:40:05 +0100

    Have working credit screen, in external scene

---
+++ ThinkTank/Assets/Scripts/ScriptCredits.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Credits : MonoBehaviour
+{
+
+    [SerializeField] GameObject credits;
+    private bool showing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showing = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreditsOff()
+    {
+        if (showing)
+        {
+            credits.SetActive(false);
+        }
+    }
+
+    public void ToggleCredits()
+    {
+        if (!credits.activeInHierarchy)
+        {
+            credits.SetActive(true);
+            showing = true;
+        }
+        else if (credits.activeInHierarchy)
+        {
+            credits.SetActive(false);
+            showing = false;
+        }
+    }
+}
